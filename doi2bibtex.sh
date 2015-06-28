760bba03812f4fa261d743584f80ff9c54be7968
Jan Hermann <jan@janhermann.cz>
2015-06-28 14:43:44 +0200

    update test

--- doi2bibtex.sh
+++ doi2bibtex.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 dir=`dirname $0`
-cat | python3 $dir/get_bibtex.py | bibtool -r $dir/bibtool.conf
+cat \
+    | python3 $dir/get_bibtex.py \
+    | python3 $dir/do_abbrv.py \
+    | bibtool -r $dir/bibtool.conf
