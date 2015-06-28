7724231c24fd2b85bb1e3d6aa94bf60735886767
Jan Hermann <jan@janhermann.cz>
2015-06-28 14:34:51 +0200

    working do_abbrev.py

--- do_abbrv.py
+++ do_abbrv.py
@@ -1,18 +1,58 @@
 #!/usr/bin/env python3
-import bibtexparser as bibparse
+import bibtexparser as bibtex
 import sqlite3 as sql
 import sys
+from collections import namedtuple
+import re
 
-conn = sql.connect('jabbrv.sqlite')
+
+ignored = ['of', 'the']
+corrections = {
+    'Nat': 'Nature',
+    'Comms': 'Communications'
+}
+
+Recd = namedtuple('Recd', ('full', 'abbr', 'lang', 'type'))
+
+db_path = 'jabbrv.db'
+conn = sql.connect(db_path)
 cur = conn.cursor()
-bib = bibparse.load(sys.stdin)
+
+
+def shorten(word):
+    word, suff = re.findall(r'([^:]+)(.*)', word)[0]
+    is_capital = word[0].isupper()
+    recds = [Recd(*abbr) for abbr in
+             cur.execute('select * from abbrvs where ? like full', (word,))]
+    if recds:
+        if len(recds) > 1:
+            fulls = [r for r in recds if r.type == 0]
+            if len(fulls) == 1:
+                # if only one non-affix abbreviation
+                recds = fulls
+            elif all(r.abbr == recds[0].abbr for r in recds):
+                # if all abbreviations equivalent
+                pass
+            else:
+                for recd in recds:
+                    print(recd)
+                raise Exception('Multiple nonequivalent abbreviations possible')
+        abbr = recds[0].abbr
+        if abbr != 'n.a.':
+            word = abbr
+    return (word.capitalize() if is_capital else word) + suff
+
+
+def process(title):
+    words = [corrections.get(w, w) for w in title.split()
+             if w.lower() not in ignored]
+    if len(words) > 1:
+        words = [shorten(word) if not word.endswith('.') else word
+                 for word in words]
+    return ' '.join(words)
+
+
+bib = bibtex.load(sys.stdin)
 for item in bib.entries:
-    journal = item['journal']
-    if cur.execute('select * from abbrvs where abbrev=?',
-                   (journal.replace('.', '').upper(),)).fetchone():
-        print('ABBREVIATED')
-        continue  # already abbreviated
-    abbrev = cur.execute('select abbrev from abbrvs where full=?',
-                         (journal.upper(),)).fetchone()
-    abbrev = abbrev.capitalize()
-    print(journal, abbrev)
+    item['journal'] = process(item['journal'])
+bibtex.dump(bib, sys.stdout)
