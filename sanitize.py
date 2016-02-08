09983267ebffee0157f08408cea0c70e2b1a5c46
Jan Hermann <jan@janhermann.cz>
2016-02-08 19:59:25 +0100

    do not rearrange author field

--- sanitize.py
+++ sanitize.py
@@ -15,9 +15,6 @@ bib = bibtex.load(sys.stdin)
 for item in bib.entries:
     if 'journaltitle' in item and 'journal' not in item:
         item['journal'] = item['journaltitle']
-    authors = item['author'].split(' and ')
-    authors = [a if ',' not in a else ' '.join(reversed(a.split(', '))) for a in authors]
-    item['author'] = ' and '.join(authors)
     if 'date' in item and 'year' not in item:
         item['year'] = item['date'].split('-')[0]
     if 'doi' in item:
